da1cc2089b2f929b4e3263c56a3a0b1c1c68c062
briancosentino <cosentinobrian@gmail.com>
2025-02-26 15:30:33 +0100

    first exercise

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,5 +1,14 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-console.log(names);
+
+//map version
+const stringNames = names.map(name=>{
+    return name
+}).join(',')
+console.log(stringNames);
+//forEach version
+ const namesExtended = names.forEach(name=> {console.log(name)}
+ )
+
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
