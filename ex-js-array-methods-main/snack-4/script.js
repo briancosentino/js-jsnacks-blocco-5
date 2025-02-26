a9a5a7728be69b80bbc8df538f8d73df101c2df6
briancosentino <cosentinobrian@gmail.com>
2025-02-26 15:07:44 +0100

    add exercise from 1 to 5

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -5,5 +5,10 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const author = posts.map(post=>{
+  return post.author
+})
+console.log(author);
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
